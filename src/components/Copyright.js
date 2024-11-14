5ed8df03dc0a9d61775814e4a3fcc4a08bc51987
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-11-14 14:58:51 +0500

    react

---
+++ src/components/Copyright.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Copyright = () => {
+  return (
+    <div className='border-t border-white/20'>
+      <div className='container mx-auto'>
+        <div className='py-6'>
+          <p className='text-base text-center'>
+            Copyright &copy; 2024 Bistro Cafe. All rights reserved.
+          </p>
+          <p className='text-base text-center font-extrabold italic'>Designed by Zara Yousuf</p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Copyright;
